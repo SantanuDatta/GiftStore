45ed1ef2255768d8d6bb2b76e7eb1f52ebd7e845
SantanuDatta <santanudatta94@gmail.com>
2023-04-09 12:02:35 +0600

    wip

--- public/backend/js/table-datatable.js
+++ public/backend/js/table-datatable.js
@@ -1,20 +1,19 @@
-$(function() {
-	"use strict";
+$(function () {
+    "use strict";
 
-    $(document).ready(function() {
-        $('#data').DataTable();
-    } );
+    $(document).ready(function () {
+        $("#data").DataTable();
+    });
 
-
-      $(document).ready(function() {
-        var table = $('#example2').DataTable( {
+    $(document).ready(function () {
+        var table = $("#example2").DataTable({
             lengthChange: false,
-            buttons: [ 'copy', 'excel', 'pdf', 'print']
-        } );
-     
-        table.buttons().container()
-            .appendTo( '#example2_wrapper .col-md-6:eq(0)' );
-    } );
+            buttons: ["copy", "excel", "pdf", "print"],
+        });
 
-
+        table
+            .buttons()
+            .container()
+            .appendTo("#example2_wrapper .col-md-6:eq(0)");
+    });
 });
